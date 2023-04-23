7093017fcea5b1a7ed4168ef67ee5cdcb19ba1d4
Sajith Ali S <119950449+Sajith-Ali-S@users.noreply.github.com>
2023-04-23 08:08:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
 // Your Script here.
 
 const lookup = {
-  'A': 'N','B': 'O','C': 'P','D': 'Q',
-  'E': 'R','F': 'S','G': 'T','H': 'U',
-  'I': 'V','J': 'W','K': 'X','L': 'Y',
-  'M': 'Z','N': 'A','O': 'B','P': 'C',
-  'Q': 'D','R': 'E','S': 'F','T': 'G',
-  'U': 'H','V': 'I','W': 'J','X': 'K',
-  'Y': 'L','Z': 'M', '?': '?', ',': ','
-};
+    'A': 'N','B': 'O','C': 'P','D': 'Q',
+    'E': 'R','F': 'S','G': 'T','H': 'U',
+    'I': 'V','J': 'W','K': 'X','L': 'Y',
+    'M': 'Z','N': 'A','O': 'B','P': 'C',
+    'Q': 'D','R': 'E','S': 'F','T': 'G',
+    'U': 'H','V': 'I','W': 'J','X': 'K',
+    'Y': 'L','Z': 'M', '?': '?', ',': ','
+  };
+//   for(let i in lookup){
+//     console.log(lookup["A"])
+//   }
+  function rot13(encodedStr){
+     let decodedArr = []; // Your Result goes here
+    // Only change code below this line
+      for(let i =0; i< encodedStr.length ; i++){
+          let currentChar = encodedStr.charAt(i);
+          let isPresent = false
+          for(let key in lookup){
+              if(key == currentChar){
+                  decodedArr[i]= lookup[currentChar];
+                  isPresent = true
+                   }
+                       }
+                    if(isPresent==false){
+                      decodedArr[i] = currentChar
+                    }
 
-function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-	for(let i =0; i< encodedStr.length ; i++){
-		const currentChar = encodedStr.charAt(i);
-		for(let key in lookup){
-			if(key == currentChar){
-				decodedArr[i]= lookup.currentChar;
-					}
-			else{
-				decodedArr[i] = currentChar;
-			}
-	}
-	}
-  return decodedArr ;//return decodedArr
-}
-
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-console.log(rot13("SERR YBIR? NPPVBWBO"));
-
+                    //decodedArr[i]= currentChar;
+      }
+     return decodedArr ;//return decodedArr
+  }
+  
+  // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+  
+  console.log(rot13("SERR YBIR? NPPVBWBO"));
 module.exports = rot13;
